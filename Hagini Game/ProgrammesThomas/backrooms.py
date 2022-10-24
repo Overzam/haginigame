fb63e1f61c2ea182e11e6d801fe225a06e5bc468
Overzam <103412303+Overzam@users.noreply.github.com>
2022-10-24 13:03:15 +0200

    backroom

--- Hagini Game/ProgrammesThomas/backrooms.py
+++ Hagini Game/ProgrammesThomas/backrooms.py
@@ -8,11 +8,13 @@ pyg.font.init()
 pyg.display.set_caption('hagini game')
 screen = pyg.display.set_mode((width, height))
 clock = pyg.time.Clock()
-background = pyg.image.load("image/backroom.png")
-background = pyg.transform.scale(background, (height, width))
+background = pyg.image.load("image/backroom.jpg")
 fond = background.convert()
 screen.blit(fond, (0, 0))
-pyg.display.flip()
+pyg.font.init()
+font = pyg.font.Font("../../Pixellari.ttf", 72)
+text = font.render("Tibaka", True, (0, 128, 0))
+xtext = height/5
 
 def quit():
     if event.type == pyg.KEYDOWN :
@@ -20,11 +22,18 @@ def quit():
             pyg.quit()
             exit()
 
+
 while True:
     for event in pyg.event.get():
         if event.type == pyg.KEYDOWN:
-            if event.key == pyg.K_a :
-                print("sus")
+            if event.key == pyg.K_z :
+                screen.blit(text, (xtext, width / 5))
+                xtext += height/100
+                print(4)
+                screen.blit(fond, (0, 0))
             if event.key == pyg.K_ESCAPE :
                 pyg.quit()
                 exit()
+    screen.blit(text, (xtext, width / 5))
+    pyg.display.flip()
+    pyg.display.update()
