71493b0aee488b88b7032bc3cf0d62d916a5f4b6
aioIia <73951461+aioIia@users.noreply.github.com>
2023-01-12 23:55:49 +0100

    Add files via upload

---
+++ voisin.py
@@ -0,0 +1,28 @@
+import pygame as pyg
+
+width, height = 1920, 1080
+screen = pyg.display.set_mode((width, height))
+pyg.init()
+
+voisin_sprite = pyg.image.load('img/voisin.png')
+voisin_sprite = pyg.transform.scale(voisin_sprite, (400, 200))
+rouge = (255, 0, 0)
+vert = (0, 255, 0)
+
+class Voisin:
+    def __init__(self, pos_x, pos_y, pv):
+        self.x = int(pos_x)
+        self.y = int(pos_y)
+        self.pv = int(pv)
+        self.max_pv = int(pv)
+
+    def draw(self, screen):
+        screen.blit(voisin_sprite, (self.x, self.y))
+
+    def barre_de_vie(self, x, y):
+        if self.pv > self.max_pv:
+            self.pv = self.max_pv
+        ratio_barre_de_vie = self.pv / self.max_pv
+        pyg.draw.rect(screen, rouge, (x, y, 150, 20))
+        pyg.draw.rect(screen, vert, (x, y, 150 * ratio_barre_de_vie, 20))
+        
